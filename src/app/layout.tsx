6e123b171200d1cf4fc2401d266919bae13d103c
Poon <zeepengineer@gmail.com>
2024-09-11 17:55:23 +0700

    does this actually gonna work???

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 
 import { MantineProvider } from "@mantine/core";
